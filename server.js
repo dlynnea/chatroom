6b03697e768c972eb7a6ef16195ca6173135a771
dlynnea <danimatton@gmail.com>
2020-03-31 16:35:25 -0600

    cleanup

--- server.js
+++ server.js
@@ -42,4 +42,4 @@ io.on('connection', socket => {
 });
 
 const PORT = process.env.PORT || 3030;
-server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
