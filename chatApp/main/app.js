3e8b7c40f4f9d9a83bda6e845b6f8e044db2be90
dlynnea <danimatton@gmail.com>
2020-04-22 16:16:46 -0600

    fixed previous styling changes

--- chatApp/main/app.js
+++ chatApp/main/app.js
@@ -45,3 +45,32 @@ function roomOutput(room) {
 function userOutput(users) {
     friends.innerHTML = `${users.map(user => `<li>${user.username}</li>`).join('')}`
 }
+
+
+
+
+////
+
+// $(document).ready(function(){
+//     $('#message-box').keypress((e)=>{
+//       if(e.which!=13){
+//         typing=true
+//         socket.emit('typing', {user:user, typing:true})
+//         clearTimeout(timeout)
+//         timeout=setTimeout(typingTimeout, 3000)
+//       }else{
+//         clearTimeout(timeout)
+//         typingTimeout()
+//         //sendMessage() function will be called once the user hits enter
+//         sendMessage()
+//       }
+//     })
+
+//     //code explained later
+//     socket.on('display', (data)=>{
+//       if(data.typing==true)
+//         $('.typing').text(`${data.user} is typing...`)
+//       else
+//         $('.typing').text("")
+//     })
+// })
